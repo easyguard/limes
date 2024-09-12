d16f7809b0efd0709a96e9209df3dd53d47bcb14
Cfp <chaosflo44plus@gmail.com>
2024-09-12 17:20:13 +0200

    ✨ Ruleset support

---
+++ src/config.rs
@@ -0,0 +1,54 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Ruleset {
+	pub zones: Vec<Zone>,
+	pub forwards: Vec<Forward>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Zone {
+	pub name: String,
+	pub input: Chain,
+	pub output: Chain,
+	pub forward: Vec<ForwardItem>,
+	pub items: Items
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Chain {
+	pub ports: Option<Vec<PortRule>>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ForwardItem {
+	pub dest: String,
+	pub ports: Vec<PortRule>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PortRule {
+	pub protocol: String,
+	pub r#type: Option<String>,
+	pub limit: Option<String>,
+	pub port: Option<u16>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Items {
+	pub interfaces: Vec<String>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Forward {
+	pub src: String,
+	pub dest: String,
+	pub dest_ip: String,
+	pub port: u16,
+	pub protocol: String,
+}
+
+pub fn readRuleset(file: &str) -> Result<Ruleset, std::io::Error> {
+	let ruleset = std::fs::read_to_string(file)?;
+	Ok(serde_json::from_str(&ruleset)?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod nftutils;
+mod config;
 
 use std::str::FromStr;
 
+use config::Ruleset;
 use ipnet::IpNet;
 use nftables::{batch::Batch, expr, helper, schema, stmt, types};
 
@@ -13,16 +15,19 @@ const NAT_TABLE_FAMILY: types::NfFamily = types::NfFamily::INet;
 const NAT_TABLE_NAME: &str = "nat";
 
 fn main() {
-	let ruleset = generate_ruleset();
+	let config = config::readRuleset("/root/firewall.json").expect("Failed to read ruleset! Is it formatted correctly?");
+	let ruleset = generate_ruleset(&config);
 	helper::apply_ruleset(&ruleset, None, None).unwrap();
+	// let nftables = serde_json::to_string(&ruleset).expect("failed to serialize Nftables struct");
+	// println!("{}", nftables);
 }
 
-fn generate_ruleset() -> schema::Nftables {
+fn generate_ruleset(config: &Ruleset) -> schema::Nftables {
 	let mut batch = Batch::new();
 	// flush command
 	batch.add_cmd(schema::NfCmd::Flush(schema::FlushObject::Ruleset(None)));
 
-	generate_filter_table(&mut batch);
+	generate_filter_table(&mut batch, config);
 	generate_nat_table(&mut batch);
 	
 	batch.to_nftables()
@@ -32,17 +37,17 @@ fn generate_ruleset() -> schema::Nftables {
 // INET FILTER TABLE
 // =================
 
-fn generate_filter_table(batch: &mut Batch) {
+fn generate_filter_table(batch: &mut Batch, config: &Ruleset) {
 	batch.add(schema::NfListObject::Table(schema::Table::new(
 		FILTER_TABLE_FAMILY,
 		FILTER_TABLE_NAME.to_string()
 	)));
 
-	generate_filter_input_chain(batch);
-	generate_filter_forward_chain(batch);
+	generate_filter_input_chain(batch, config);
+	generate_filter_forward_chain(batch, config);
 }
 
-fn generate_filter_input_chain(batch: &mut Batch) {
+fn generate_filter_input_chain(batch: &mut Batch, config: &Ruleset) {
 	batch.add(schema::NfListObject::Chain(schema::Chain::new(
 		FILTER_TABLE_FAMILY,
 		FILTER_TABLE_NAME.to_string(),
@@ -135,17 +140,94 @@ fn generate_filter_input_chain(batch: &mut Batch) {
 	)));
 
 	// TEMP: Allow all traffic
-	batch.add(schema::NfListObject::Rule(schema::Rule::new(
-		FILTER_TABLE_FAMILY,
-		FILTER_TABLE_NAME.to_string(),
-		FILTER_INPUT_CHAIN_NAME.to_string(),
-		vec![
-			stmt::Statement::Accept(None)
-		]
-	)));
+	// batch.add(schema::NfListObject::Rule(schema::Rule::new(
+	// 	FILTER_TABLE_FAMILY,
+	// 	FILTER_TABLE_NAME.to_string(),
+	// 	FILTER_INPUT_CHAIN_NAME.to_string(),
+	// 	vec![
+	// 		stmt::Statement::Accept(None)
+	// 	]
+	// )));
+
+	for zone in &config.zones {
+		if zone.input.ports.is_none() {
+			continue;
+		}
+		println!("[filter input] Generating chain for {} zone", zone.name);
+		batch.add(schema::NfListObject::Chain(schema::Chain::new(
+			FILTER_TABLE_FAMILY,
+			FILTER_TABLE_NAME.to_string(),
+			format!("input_{}", zone.name),
+			None,
+			None,
+			None,
+			None,
+			None
+		)));
+		let ifs: Vec<expr::SetItem> = zone.items.interfaces.iter().map(|interface| {
+				expr::SetItem::Element(expr::Expression::String(interface.clone()))
+			}).collect();
+		println!("[filter input] Interfaces: {:?}", ifs);
+		batch.add(schema::NfListObject::Rule(schema::Rule::new(
+			FILTER_TABLE_FAMILY,
+			FILTER_TABLE_NAME.to_string(),
+			FILTER_INPUT_CHAIN_NAME.to_string(),
+			vec![
+				stmt::Statement::Match(stmt::Match {
+					left: expr::Expression::Named(expr::NamedExpression::Meta(
+						expr::Meta {
+							key: expr::MetaKey::Iifname
+						}
+					)),
+					right: expr::Expression::Named(expr::NamedExpression::Set(ifs)),
+					op: stmt::Operator::IN
+				}),
+				stmt::Statement::Jump(stmt::JumpTarget {
+					target: format!("input_{}", zone.name),
+				})
+			]
+		)));
+		let port_rules = zone.input.ports.as_ref().unwrap();
+		for rule in port_rules {
+			let mut expr: Vec<stmt::Statement> = Vec::new();
+			if rule.protocol == "icmp" {
+				panic!("ICMP not supported yet!");
+			} else {
+				if rule.limit.is_some() {
+					panic!("Limit not supported yet!");
+				}
+				expr.push(stmt::Statement::Match(stmt::Match {
+					left: expr::Expression::Named(expr::NamedExpression::Meta(
+						expr::Meta {
+							key: expr::MetaKey::L4proto
+						}
+					)),
+					right: expr::Expression::String(rule.protocol.clone()),
+					op: stmt::Operator::EQ
+				}));
+				expr.push(stmt::Statement::Match(stmt::Match {
+					left: expr::Expression::Named(expr::NamedExpression::Payload(
+						expr::Payload::PayloadField(expr::PayloadField {
+							protocol: "th".to_string(),
+							field: "dport".to_string()
+						})
+					)),
+					right: expr::Expression::Number(rule.port.unwrap() as u32),
+					op: stmt::Operator::EQ
+				}));
+				expr.push(stmt::Statement::Accept(None));
+			}
+			batch.add(schema::NfListObject::Rule(schema::Rule::new(
+				FILTER_TABLE_FAMILY,
+				FILTER_TABLE_NAME.to_string(),
+				format!("input_{}", zone.name),
+				expr
+			)))
+		}
+	}
 }
 
-fn generate_filter_forward_chain(batch: &mut Batch) {
+fn generate_filter_forward_chain(batch: &mut Batch, config: &Ruleset) {
 	batch.add(schema::NfListObject::Chain(schema::Chain::new(
 		FILTER_TABLE_FAMILY,
 		FILTER_TABLE_NAME.to_string(),
@@ -214,23 +296,115 @@ fn generate_filter_forward_chain(batch: &mut Batch) {
 		]
 	)));
 
-	batch.add(schema::NfListObject::Rule(schema::Rule::new(
-		FILTER_TABLE_FAMILY,
-		FILTER_TABLE_NAME.to_string(),
-		FILTER_FORWARD_CHAIN_NAME.to_string(),
-		vec![
-			stmt::Statement::Match(stmt::Match {
-				left: expr::Expression::Named(expr::NamedExpression::Meta(
-					expr::Meta {
-						key: expr::MetaKey::Iifname
+	// TEMP: Allow all traffic
+	// batch.add(schema::NfListObject::Rule(schema::Rule::new(
+	// 	FILTER_TABLE_FAMILY,
+	// 	FILTER_TABLE_NAME.to_string(),
+	// 	FILTER_FORWARD_CHAIN_NAME.to_string(),
+	// 	vec![
+	// 		stmt::Statement::Match(stmt::Match {
+	// 			left: expr::Expression::Named(expr::NamedExpression::Meta(
+	// 				expr::Meta {
+	// 					key: expr::MetaKey::Iifname
+	// 				}
+	// 			)),
+	// 			right: expr::Expression::String("eth1".to_string()),
+	// 			op: stmt::Operator::EQ
+	// 		}),
+	// 		stmt::Statement::Accept(None)
+	// 	]
+	// )));
+
+	for zone in &config.zones {
+		if zone.forward.is_empty() {
+			continue;
+		}
+		println!("[filter forward] Generating chains for {} zone", zone.name);
+		for subzone in &zone.forward { 
+			// subzone.dest is just the name of the destination zone, we need to get the actual interface of the zone
+			let dest_zone = config.zones.iter().find(|z| z.name == subzone.dest).unwrap();
+			let dest_if = &dest_zone.items.interfaces[0];
+			batch.add(schema::NfListObject::Chain(schema::Chain::new(
+				FILTER_TABLE_FAMILY,
+				FILTER_TABLE_NAME.to_string(),
+				format!("forward_{}_{}", zone.name, subzone.dest),
+				None,
+				None,
+				None,
+				None,
+				None
+			)));
+			let ifs: Vec<expr::SetItem> = zone.items.interfaces.iter().map(|interface| {
+					expr::SetItem::Element(expr::Expression::String(interface.clone()))
+				}).collect();
+			println!("[filter forward] Interfaces: {:?}", ifs);
+			batch.add(schema::NfListObject::Rule(schema::Rule::new(
+				FILTER_TABLE_FAMILY,
+				FILTER_TABLE_NAME.to_string(),
+				FILTER_FORWARD_CHAIN_NAME.to_string(),
+				vec![
+					stmt::Statement::Match(stmt::Match {
+						left: expr::Expression::Named(expr::NamedExpression::Meta(
+							expr::Meta {
+								key: expr::MetaKey::Iifname
+							}
+						)),
+						right: expr::Expression::Named(expr::NamedExpression::Set(ifs)),
+						op: stmt::Operator::IN
+					}),
+					stmt::Statement::Match(stmt::Match {
+						left: expr::Expression::Named(expr::NamedExpression::Meta(
+							expr::Meta {
+								key: expr::MetaKey::Oifname
+							}
+						)),
+						right: expr::Expression::String(dest_if.to_string()),
+						op: stmt::Operator::EQ
+					}),
+					stmt::Statement::Jump(stmt::JumpTarget {
+						target: format!("forward_{}_{}", zone.name, subzone.dest),
+					})
+				]
+			)));
+			let port_rules = &subzone.ports;
+			for rule in port_rules {
+				let mut expr: Vec<stmt::Statement> = Vec::new();
+				if rule.protocol == "icmp" {
+					panic!("ICMP not supported yet!");
+				} else {
+					if rule.limit.is_some() {
+						panic!("Limit not supported yet!");
 					}
-				)),
-				right: expr::Expression::String("eth1".to_string()),
-				op: stmt::Operator::EQ
-			}),
-			stmt::Statement::Accept(None)
-		]
-	)));
+					expr.push(stmt::Statement::Match(stmt::Match {
+						left: expr::Expression::Named(expr::NamedExpression::Meta(
+							expr::Meta {
+								key: expr::MetaKey::L4proto
+							}
+						)),
+						right: expr::Expression::String(rule.protocol.clone()),
+						op: stmt::Operator::EQ
+					}));
+					expr.push(stmt::Statement::Match(stmt::Match {
+						left: expr::Expression::Named(expr::NamedExpression::Payload(
+							expr::Payload::PayloadField(expr::PayloadField {
+								protocol: "th".to_string(),
+								field: "dport".to_string()
+							})
+						)),
+						right: expr::Expression::Number(rule.port.unwrap() as u32),
+						op: stmt::Operator::EQ
+					}));
+					expr.push(stmt::Statement::Accept(None));
+				}
+				batch.add(schema::NfListObject::Rule(schema::Rule::new(
+					FILTER_TABLE_FAMILY,
+					FILTER_TABLE_NAME.to_string(),
+					format!("forward_{}_{}", zone.name, subzone.dest),
+					expr
+				)))
+			}
+		}
+	}
 }
 
 // =========
